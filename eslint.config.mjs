6b9abc29592a16e5266c258a59c83a967015102e
usamah05 <usamahabdulwadud534@gmail.com>
2025-07-28 13:35:47 +0700

    user registration implemented

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,13 +6,13 @@ import daStyle from 'eslint-config-dicodingacademy';
 
 export default defineConfig([
   daStyle,
+  {
+    ignores: [
+      'node_modules/',
+      'migrations/',
+    ],
+  },
   { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
-  {
-  "parserOptions": {
-    "ecmaVersion": 2020, // Atau versi yang lebih tinggi, misalnya 2022, latest
-    "sourceType": "module"
-  },
-}
 ]);
